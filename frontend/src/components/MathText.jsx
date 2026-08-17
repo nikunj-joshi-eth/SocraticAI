6b1f24165a00c71ca836e8dbb9f2dd29acefa97e
Raghunandana <tzierrr@Raghunandanas-MacBook-Air.local>
2026-08-17 23:50:57 +0530

    Fix JS comment syntax in MathText.jsx for clean Vite production builds

--- frontend/src/components/MathText.jsx
+++ frontend/src/components/MathText.jsx
@@ -5,13 +5,13 @@ export default function MathText({ text = '' }) {
 
   // Clean format raw LaTeX markup into beautiful, human-readable Unicode text
   const cleanedText = text
-    .replace(/\\text\{([^}]+)\}/g, '$1')           # \text{m/s} -> m/s
-    .replace(/\\vec\{([^}]+)\}/g, '$1⃗')            # \vec{p} -> p⃗
-    .replace(/\\hat\{i\}/g, 'î')                    # \hat{i} -> î
-    .replace(/\\hat\{j\}/g, 'ĵ')                    # \hat{j} -> ĵ
-    .replace(/\\hat\{k\}/g, 'k̂')                    # \hat{k} -> k̂
-    .replace(/\\mathbf\{([^}]+)\}/g, '$1')         # \mathbf{x} -> x
-    .replace(/_\{([^}]+)\}/g, '_$1')               # v_{sg} -> v_sg
+    .replace(/\\text\{([^}]+)\}/g, '$1')           // \text{m/s} -> m/s
+    .replace(/\\vec\{([^}]+)\}/g, '$1⃗')            // \vec{p} -> p⃗
+    .replace(/\\hat\{i\}/g, 'î')                    // \hat{i} -> î
+    .replace(/\\hat\{j\}/g, 'ĵ')                    // \hat{j} -> ĵ
+    .replace(/\\hat\{k\}/g, 'k̂')                    // \hat{k} -> k̂
+    .replace(/\\mathbf\{([^}]+)\}/g, '$1')         // \mathbf{x} -> x
+    .replace(/_\{([^}]+)\}/g, '_$1')               // v_{sg} -> v_sg
     .replace(/\\in/g, '∈')
     .replace(/\\mathbb\{N\}/g, 'ℕ')
     .replace(/\\mathbb\{R\}/g, 'ℝ')
